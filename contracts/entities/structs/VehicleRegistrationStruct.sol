c88277992b8e70f976c57ab9712f8ca7574b6ad3
adohong4 <adohong4@gmail.com>
2025-07-25 16:59:03 +0700

    feat[vehicleRegistration]: add struct, interface && event

---
+++ contracts/entities/structs/VehicleRegistrationStruct.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "../../constants/Enum.sol";
+
+library VehicleRegistrationStruct {
+    /**
+     * @dev Struct to hold vehicle registration details
+     */
+    struct VehicleRegistration {
+        address addressUser;
+        string identityNo;
+        string vehicleModel;
+        string chassisNo;
+        string vehiclePlateNo;
+        uint256 colorPlate;
+        Enum.Status status;
+    }
+
+    /**
+     * @dev Struct for input data when registering a vehicle
+     */
+    struct RegistrationInput {
+        address addressUser;
+        string identityNo;
+        string vehicleModel;
+        string chassisNo;
+        string vehiclePlateNo;
+        uint256 colorPlate; // 0: White, 1: Yellow, 2: Green, 3: Blue, 4: Red
+    }
+
+    /**
+     * @dev Struct for updating vehicle registration details
+     */
+    struct RegistrationUpdateInput {
+        address addressUser;
+        string identityNo;
+        uint256 colorPlate;
+    }
+}
---
+++ contracts/interfaces/external/IVehicleRegistration.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "../../entities/structs/VehicleRegistrationStruct.sol";
+
+interface IVehicleRegistration {
+    //events
+    event Registration(address indexed _addressAgent, string _vehiclePlateNo, uint256 _timestamp);
+    event Update(address indexed _addressAgent, string _vehiclePlateNo, uint256 _timestamp);
+    event Revoke(address indexed _addressAgent, string _vehiclePlateNo, uint256 _timestamp);
+
+    /**
+     * @dev Registers a new vehicle
+     * @param input The registration input data
+     */
+    function registerVehicleRegistration(VehicleRegistrationStruct.RegistrationInput calldata input) external;
+
+    /**
+     * @dev Updates an existing vehicle registration
+     * @param __vehiclePlateNo The vehicle plate number to update
+     * @param input The updated registration data
+     */
+    function updateVehicleRegistration(
+        string calldata __vehiclePlateNo,
+        VehicleRegistrationStruct.RegistrationUpdateInput calldata input
+    ) external;
+
+    /**
+     * @dev Retrieves vehicle registration details by address user
+     * @param _addressUser The address user to query
+     * @return The vehicle registration details
+     */
+    function getVehicleByAddressUser(address _addressUser)
+        external
+        view
+        returns (VehicleRegistrationStruct.VehicleRegistration memory);
+
+    /**
+     * @dev Get all vehicle registrations
+     * @return An array of all vehicle registrations
+     */
+    function getAllVehicleRegistrations()
+        external
+        view
+        returns (VehicleRegistrationStruct.VehicleRegistration[] memory);
+
+    /**
+     * @dev Retrieves vehicle registration details by vehicle plate number
+     */
+    function RevokeVehicleRegistration(string memory _vehiclePlateNo) external;
+}
